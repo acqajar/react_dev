69e297358eded19f0153e3b6826436aba06e3f56
Arsames <acqajar@gmail.com>
2018-01-25 15:43:11 -0800

    reducers redux

--- client/src/App1.js
+++ client/src/App1.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class App1 extends Component {
   render() {
     return (
       <div className="App">
@@ -19,4 +19,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default App1;
---
+++ client/src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      hi there
+      <p> What is up </p>
+    </div>
+  );
+};
+
+export default App;
