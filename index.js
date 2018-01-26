9243d4be299ef0f21e5f630fe225fc23fab8d29e
Arsames <acqajar@gmail.com>
2018-01-26 11:44:38 -0800

    wire up express for charges

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const mongoose = require('mongoose');
 const keys = require('./config/keys');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 require('./models/User');
 require('./services/passport');
 mongoose.connect(keys.mongoUri);
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -18,6 +20,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
