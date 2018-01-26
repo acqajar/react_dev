9f9f2ca24c7f28e5f71b54703ad8ca6600958a7e
Arsames <acqajar@gmail.com>
2018-01-26 12:22:08 -0800

    build react for deployment

--- index.js
+++ index.js
@@ -22,5 +22,16 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  // express serve up prod files
+  // if it doesnt exist, look in client/build directory for said file
+  app.use(express.static('client/build'));
+  // any route we dont understand after looking in the client/build, serve up index.html document
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
