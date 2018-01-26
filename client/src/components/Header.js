7810532371974c901310ab19e29f60e898766c5a
Arsames <acqajar@gmail.com>
2018-01-25 23:45:45 -0800

    header logged in and out

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,36 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        // 'Still deciding';
+        return;
+      case false:
+        // logged out
+        return (
+          <li>
+            <a href="/auth/google"> Login with Google</a>
+          </li>
+        );
+      default:
+        // 'Logged in'
+        return (
+          <li>
+            <a href="/"> Logout with Google</a>
+          </li>
+        );
+    }
+  }
   render() {
+    // console.log(this.props);
     return (
       <nav>
         <div className="nav-wrapper">
           <a className="brand-logo">OurOffice</a>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li>
-              <a href="/">Login with Google</a>
-            </li>
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
@@ -17,4 +38,11 @@ class Header extends Component {
   }
 }
 
-export default Header;
+// function mapStateProps(state) {
+//   return { auth: state.auth };
+// }
+function mapStateProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateProps)(Header);
