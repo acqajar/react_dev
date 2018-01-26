429b5d7485dc73629c0f50c79791b56f4088f35a
Arsames <acqajar@gmail.com>
2018-01-26 12:08:54 -0800

    update credits as stripe purchase made

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,5 +1,21 @@
 const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSectreKey);
+const requireLogin = require('../middlewares/requireLogin');
+
 module.exports = app => {
-  app.post('/api/stripe', (req, res) => {});
+  app.post('/api/stripe', requireLogin, async (req, res) => {
+    console.log(req.body);
+
+    const charge = await stripe.charges.create({
+      amount: 5000,
+      currency: 'usd',
+      description: 'paying for subscription',
+      source: req.body.id
+    });
+    console.log(charge);
+
+    req.user.credits += 1;
+    const user = await req.user.save();
+    res.send(user);
+  });
 };
