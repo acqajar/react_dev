36654a559286d5a1f67920bee2aee764522e03e6
Arsames <acqajar@gmail.com>
2018-01-26 16:40:25 -0800

    routing express react

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,7 +2,7 @@ const passport = require('passport');
 
 module.exports = app => {
   app.get(
-    '/auth/google',
+    '/auth/google1',
     passport.authenticate('google', {
       scope: ['profile', 'email']
     })
