e7ca31a36f64cd1ce590fe899a1b0f65dccf0b35
Arsames <acqajar@gmail.com>
2018-01-26 16:37:02 -0800

    routing express react

--- client/src/index.js
+++ client/src/index.js
@@ -1,22 +1,25 @@
-import 'materialize-css/dist/css/materialize.min.css'; // if no reltaive path, assumes node_module
+import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import registerServiceWorker from './registerServiceWorker';
-import reducers from './reducers';
 import reduxThunk from 'redux-thunk';
 
+import App from './components/App';
+import reducers from './reducers';
+
+// Development only axios helpers!
+import axios from 'axios';
+window.axios = axios;
+
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.querySelector('#root')
 );
-registerServiceWorker();
 
 // console.log('Stripe key is ', process.env.REACT_APP_STRIPE_KEY);
 // console.log('Stripe key is ', process.env.NODE_ENV);
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -22,6 +22,7 @@ module.exports = app => {
   });
 
   app.get('/api/current_user', (req, res) => {
+    console.log('inside current user');
     res.send(req.user);
   });
 };
