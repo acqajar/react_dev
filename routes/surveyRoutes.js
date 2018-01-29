bf9944ed5ed728744c68a92ff45e3034de0eb5e3
Arsames <acqajar@gmail.com>
2018-01-28 17:05:03 -0800

    fixing sendgrid

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -6,23 +6,30 @@ const Mailer = require('../services/mailer');
 const surveyTemplate = require('../services/emailTemplates/surveyTemplates');
 
 module.exports = app => {
-  app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
-    const { title, body, subject, recipients } = req.body;
+  app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
+    const { title, subject, body, recipients } = req.body;
 
     const survey = new Survey({
-      // title: title, can just be title bc the same in es6
       title,
       subject,
       body,
-      // recipients: recipients.split(',').map(email => {return {email:email.trim()}})
-      // with es6, can be condensed to below
       recipients: recipients.split(',').map(email => ({ email: email.trim() })),
       _user: req.user.id,
       dateSent: Date.now()
     });
 
-    // send email
+    // Great place to send an email!
     const mailer = new Mailer(survey, surveyTemplate(survey));
-    mailer.send();
+
+    try {
+      await mailer.send();
+      await survey.save();
+      req.user.credits -= 1;
+      const user = await req.user.save();
+
+      res.send(user);
+    } catch (err) {
+      res.status(422).send(err);
+    }
   });
 };
