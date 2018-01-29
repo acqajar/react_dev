0b9eedc3c86a0bd8de5224d901d48f028d276ff7
Arsames <acqajar@gmail.com>
2018-01-29 14:41:44 -0800

    redux form

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -31,7 +31,10 @@ class SurveyForm extends Component {
       <div>
         <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
           {this.renderFields()}
-          <button>Submit</button>
+          <button className="teal btn-flat right white-text" type="submit">
+            Next
+            <i className="material-icons right">done</i>
+          </button>
         </form>
       </div>
     );
