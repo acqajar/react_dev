b842be60170ed67825bdacef970e1e6ff690e8bb
Arsames <acqajar@gmail.com>
2018-01-29 14:47:21 -0800

    redux form format

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -3,6 +3,7 @@ import _ from 'lodash';
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form'; // takes care of action creators, similar to connect helper
 import SurveyField from './SurveyField';
+import { Link } from 'react-router-dom';
 
 const FIELDS = [
   { label: 'Survey Title', name: 'title' },
@@ -31,6 +32,9 @@ class SurveyForm extends Component {
       <div>
         <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
           {this.renderFields()}
+          <Link to="/surveys" className="red btn-flat left white-text">
+            Cancel
+          </Link>
           <button className="teal btn-flat right white-text" type="submit">
             Next
             <i className="material-icons right">done</i>
