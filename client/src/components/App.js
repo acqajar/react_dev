7d552056aaa95344d5553a1dbac6dd599105c2ce
Arsames <acqajar@gmail.com>
2018-01-28 18:08:22 -0800

    survey template client side

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,6 +6,7 @@ import * as actions from '../actions';
 import Header from './Header';
 import Landing from './Landing';
 import Dashboard from './Dashboard';
+const SurveyNew = () => <h2> New Survey </h2>;
 
 class App extends Component {
   componentDidMount() {
@@ -20,7 +21,7 @@ class App extends Component {
             <Header />
             <Route exact path="/" component={Landing} />
             <Route exact path="/surveys" component={Dashboard} />
-            <Route path="/surveys/new" component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyNew} />
           </div>
         </BrowserRouter>
       </div>
