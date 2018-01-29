b02a74922b908675c98e1c4e30902aef20ef07e2
Arsames <acqajar@gmail.com>
2018-01-28 16:44:35 -0800

    test sendgrid

--- services/emailTemplates/surveyTemplates.js
+++ services/emailTemplates/surveyTemplates.js
@@ -1,3 +1,24 @@
+const keys = require('../../config/keys');
+// ${keys.redirectDomain}/api/surveys/${
+// survey.id
+// }/yes
+// ${keys.redirectDomain}/api/surveys/${survey.id}/no
 module.exports = survey => {
-  return '<div>' + survey.body + '</div>';
+  return `
+    <html>
+      <body>
+        <div style="text-align: center;">
+          <h3>I'd like your input!</h3>
+          <p>Please answer the following question:</p>
+          <p>${survey.body}</p>
+          <div>
+            <a href="">Yes</a>
+          </div>
+          <div>
+            <a href="">No</a>
+          </div>
+        </div>
+      </body>
+    </html>
+  `;
 };
