d2014babc837b24093c46775c918189fee196fd2
Arsames <acqajar@gmail.com>
2018-01-30 16:57:45 -0800

    webhooks working

--- sendgrid_webhook.sh
+++ sendgrid_webhook.sh
@@ -1,5 +1,5 @@
 function localtunnel {
-  lt -s l8a2oxzklqq --port 5000
+  lt -s l8a2oxzklqqzzzzzzzzzz9999koklkaindh837646619rnbfbayyu19037 --port 5000
 }
 until localtunnel; do
 echo "localtunnel server crashed"
