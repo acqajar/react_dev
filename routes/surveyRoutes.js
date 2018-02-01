34bd89def33b0f09efdc8ed243c6049e4254ca83
Arsames <acqajar@gmail.com>
2018-02-01 10:44:00 -0800

    survey routes

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,27 +1,35 @@
-const mongoose = require('mongoose');
 const _ = require('lodash');
 const Path = require('path-parser');
 const { URL } = require('url');
+const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
-const Survey = mongoose.model('surveys');
-const Mailer = require('../services/mailer');
+const Mailer = require('../services/Mailer');
 const surveyTemplate = require('../services/emailTemplates/surveyTemplates');
 
+const Survey = mongoose.model('surveys');
+
 module.exports = app => {
+  app.get('/api/surveys', requireLogin, async (req, res) => {
+    const surveys = await Survey.find({ _user: req.user.id }).select({
+      recipients: false
+    });
+
+    res.send(surveys);
+  });
+
+  app.get('/api/surveys/:surveyId/:choice', (req, res) => {
+    res.send('Thanks for voting!');
+  });
+
   app.post('/api/surveys/webhooks', (req, res) => {
     const p = new Path('/api/surveys/:surveyId/:choice');
 
     _.chain(req.body)
-      .map(req.body, ({ email, url }) => {
+      .map(({ email, url }) => {
         const match = p.test(new URL(url).pathname);
-        // cant destructure match bc p.test() might return null
         if (match) {
-          return {
-            email,
-            surveyId: match.surveyId,
-            choice: match.choice
-          };
+          return { email, surveyId: match.surveyId, choice: match.choice };
         }
       })
       .compact()
@@ -30,19 +38,20 @@ module.exports = app => {
         Survey.updateOne(
           {
             _id: surveyId,
-            recipients: { $elemMatch: { email: email, responded: false } }
+            recipients: {
+              $elemMatch: { email: email, responded: false }
+            }
           },
           {
             $inc: { [choice]: 1 },
-            $set: { 'recipients.$.responded': true }
+            $set: { 'recipients.$.responded': true },
+            lastResponded: new Date()
           }
         ).exec();
       })
       .value();
-  });
 
-  app.get('/api/surveys/:surveyId/:choice', (req, res) => {
-    res.send('Thanks for voting!');
+    res.send({});
   });
 
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
@@ -63,9 +72,9 @@ module.exports = app => {
     try {
       await mailer.send();
       await survey.save();
-      // req.user.credits -= 1;
+      req.user.credits -= 1;
       const user = await req.user.save();
-      console.log('success');
+
       res.send(user);
     } catch (err) {
       res.status(422).send(err);
